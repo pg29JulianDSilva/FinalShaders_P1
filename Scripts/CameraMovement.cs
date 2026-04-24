e93bacf35cb8022619a13669a5a0f75a563ca1d3
pg29JulianDSilva <pg29julians@vfs.com>
2026-04-24 15:37:05 -0700

    MODIFIED: Added camera movement

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera m_Camera;
+    public Transform[] m_CameraTarget;
     void Start()
     {
-        
+        m_Camera = Camera.main;
+        if (m_Camera == null) return;
+        if (m_CameraTarget[0] == null) return;
+        m_Camera.transform.position = m_CameraTarget[0].transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F1)) m_Camera.transform.position = m_CameraTarget[0].transform.position;
+        if (Input.GetKeyDown(KeyCode.F2)) m_Camera.transform.position = m_CameraTarget[1].transform.position;
+        if (Input.GetKeyDown(KeyCode.F3)) m_Camera.transform.position = m_CameraTarget[2].transform.position;
     }
 }
